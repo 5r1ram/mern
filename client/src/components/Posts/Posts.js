8990cee8cc2e9e67d8cfd16f6564e1ade4718632
5r1ram <sriramiiit33@gmail.com>
2024-09-30 14:11:11 -0400

    Fetch Posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post.js";
+
 import useStyles from "./styles.js";
 
 import { useSelector } from "react-redux";
@@ -11,12 +13,16 @@ const Posts = () => {
   console.log(posts);
 
   return (
-    <>
-        <div>Posts</div>
-        <Post />
-        <Post />
-    </>
-  )
-}
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems='stretch' spacing={3} >
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
+};
 
 export default Posts;
